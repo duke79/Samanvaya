06302ff8f7e35da2feb1a3b8b3bcf77586cec3d9
duke79 <pulkitsingh01@gmail.com>
2018-10-06 17:38:02 +0530

    flask-migrate | flask db init

--- src/flask/app/__init__.py
+++ src/flask/app/__init__.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from app.data.config import Config
 from app.data.tables.alchemy_base import AlchemyBase, db_uri, db_session
@@ -17,6 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 if config["debug"]:
     app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app, model_class=AlchemyBase)
+migrate = Migrate(app, db)
 # db.create_all() # No use until the model classes are imported. Use scripts.create_db_schema instead.
 
 # @app.after_request
